336d1716ba5af296323aea0313c08fc7d94b66cf
cecceccu <mickael.lanfranchi2a@gmail.com>
2021-09-23 14:03:53 +0200

    8. Notifications Push - résultat

--- sw.js
+++ sw.js
@@ -25,7 +25,7 @@ self.addEventListener('install', (evt) => {
 });
 
 	
-// 7.3 Notifications persistantes (envoyées depuis le service worker)
+/*// 7.3 Notifications persistantes (envoyées depuis le service worker)
 self.registration.showNotification("Notification du SW", {
     body:"je suis une notification dite persistante",
   
@@ -55,6 +55,20 @@ self.addEventListener("notificationclick", evt => {
 
     // 7.5 Fermer programmatiquement une notification
     evt.notification.close();
+})*/
+
+// 8.1 Intercepter une notification push
+self.addEventListener("push", evt => {
+    console.log("push event", evt);
+    console.log("data envoyée par la push notification :", evt.data.text());
+
+    // 8.1 afficher son contenu dans une notification
+    const title = evt.data.text();
+    const objNotification = {
+        body: "ça fonctionne", 
+        icon : "images/icons/icon-72x72.png"
+    };
+    self.registration.showNotification(title, objNotification);
 })
 
 self.addEventListener('install', (evt) => {
